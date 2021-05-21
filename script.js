58a9f9ad5c910e0c43579b949f88840c2d28ed96
Adriel Dias <you@example.com>
2021-05-21 20:03:24 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+//1
+function testingMensage(Mensage){
+    console.log(`${Mensage} budddy, whats up?`)
+}
+testingMensage(`Hello`)
+
+//2
+function testingName(nome){
+    console.log(nome)
+}
+testingName(`Adriel`)
+
+//3
+function myProfile(name, age, kindOfMusic){
+    console.log(`Meu nome é ${name}, tenho ${age} anos, e gosto do genero musical favorito se chama ${kindOfMusic}.`)
+}
+myProfile(`Adriel`,`18`,`Folk`)
+
+//4
+function favorites(movie, music){
+    console.log(`Meu filme favorito é ${movie} e minha musica favorita é ${music}`)
+}
+favorites(`No Game No Life: 0`, `Never Fall in Love Again`)
+
+//5
+function number(x){
+    console.log(x*3)
+    return x*3
+}
+number(10)
